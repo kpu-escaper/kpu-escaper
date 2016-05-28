062b8b652c4267ed53bab8b6744da702ee7c1d7f
900903ksh <900903ksh@naver.com>
2016-05-28 22:04:55 +0900

    FlameThrowerTrap 프리펩을 생성해서 트렙의 기본적인 틀을 구성하고 FlameThrowerTrapManager 스크립트를 컴포넌트로 추가

---
+++ kpu-escaper/Assets/Scripts/Trap/FlameThrowerTrap/FlameThrowerTrapManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlameThrowerTrapManager : RoomPropertyController {
+
+	public override void EnterRoom(){
+		StartCoroutine("FlameThrowerTrapRoutine");
+	}
+	public override void Update(){
+		
+	}
+	
+	public override void LeaveRoom(){
+
+	}
+
+	IEnumerator FlameThrowerTrapRoutine(){
+		yield return new WaitForSeconds (1);
+	}
+}
