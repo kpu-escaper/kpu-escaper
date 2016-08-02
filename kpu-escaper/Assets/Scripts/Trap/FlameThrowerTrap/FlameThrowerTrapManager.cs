c3789c26fc7779970803b3c47a5dd8fa41b69027
900903ksh <900903ksh@naver.com>
2016-08-02 23:24:26 +0900

    에이밍 메인게임의 틀 형성

--- kpu-escaper/Assets/Scripts/Trap/FlameThrowerTrap/FlameThrowerTrapManager.cs
+++ kpu-escaper/Assets/Scripts/Trap/FlameThrowerTrap/FlameThrowerTrapManager.cs
@@ -3,18 +3,80 @@ using System.Collections;
 
 public class FlameThrowerTrapManager : RoomPropertyController {
 
+	public GameObject FlamethrowerEffect;
+	GameObject[] Trap;
+	Vector3 holePos;
+	Quaternion angle;
+
+	private AudioSource EsAudio;
+	public AudioClip FlameThrower_Sound;
+
 	public override void EnterRoom(){
+
+		this.EsAudio = this.gameObject.AddComponent<AudioSource> ();
+		this.EsAudio.clip = this.FlameThrower_Sound;
+		this.EsAudio.loop = false;
+
+
+		Trap = GameObject.FindGameObjectsWithTag("FlameHole");
 		StartCoroutine("FlameThrowerTrapRoutine");
 	}
 	public override void Update(){
-		
+
 	}
 	
 	public override void LeaveRoom(){
-
+		StopCoroutine("FlameThrowerTrapRoutine");
 	}
 
 	IEnumerator FlameThrowerTrapRoutine(){
-		yield return new WaitForSeconds (1);
+
+		while(true)
+		{
+
+			for (int i = 0; i < 3; ++i)
+			{
+				int randomInt = Random.Range(0, Trap.Length);
+				//thornPos = new Vector3(Random.Range (-1106, 890),0,Random.Range (-980,1207));
+				holePos = new Vector3(Random.Range (-1250, 1510),0,Random.Range (-1470,1370));
+				holePos = holePos/1000;
+				Trap[randomInt].transform.localPosition = holePos;
+				Trap[randomInt].GetComponent<Animation>()["FlameHoleAni"].speed = 1;
+				Trap[randomInt].GetComponent<Animation>().Play();
+
+				switch(Trap[randomInt].layer){
+				case 8: //bottom
+					angle = Quaternion.Euler(270.0f, 0, 0);
+					break;
+				case 9: //back
+					angle = Quaternion.Euler(0, 0, 0);
+					break;
+				case 10: //top
+					angle = Quaternion.Euler(90.0f, 0, 0);
+					break;
+				case 11: //front
+					angle = Quaternion.Euler(180.0f, 0, 0);
+					break;
+				case 12: //left
+					angle = Quaternion.Euler(0, 90.0f, 0);
+					break;
+				case 13: //right
+					angle = Quaternion.Euler(0, 270.0f, 0);
+					break;
+				}
+
+				Instantiate (FlamethrowerEffect, new Vector3 (Trap[randomInt].transform.position.x,
+				                                              Trap[randomInt].transform.position.y,
+				                                              Trap[randomInt].transform.position.z), angle);
+
+			}
+			yield return new WaitForSeconds(3.1f);
+			this.EsAudio.Play ();
+			yield return new WaitForSeconds(6.1f);
+
+			
+		}
+
+
 	}
 }
---
+++ kpu-escaper/Assets/Scripts/Trap/LightningTrap/LightningTrap.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightningTrap : RoomPropertyController
+{
+	public GameObject LightningEffect;	// 번개 이펙트
+	public GameObject Foot_Circle;		// 번개칠 자리에 표시해주는 이펙트
+	public GameObject Char;				// 캐릭터
+	private Vector3 FootCirclePos1;		// 번개칠 위치
+	private Quaternion FootCirclePos2;	// 번개칠 위치의 각도
+
+	private AudioSource EsAudio;		// 오디오 플레이어
+	private AudioSource EsAudio2;		// 오디오 플레이어
+
+	public AudioClip Lightning_Sound;	// 번개 소리
+	public AudioClip Needle_Hit;		// 번개 쳐맞을때 소리
+
+	
+	
+	public override void EnterRoom()
+	{
+
+
+	}
+	
+	public override void Update()
+	{
+	}
+	
+	public override void LeaveRoom()
+	{
+		Debug.Log ("나갔습니다");
+		
+	}
+	
+	void Awake()
+	{
+
+		this.EsAudio = this.gameObject.AddComponent<AudioSource> ();
+		this.EsAudio.clip = this.Needle_Hit;
+		this.EsAudio.loop = false;
+		
+		this.EsAudio2 = this.gameObject.AddComponent<AudioSource> ();
+		this.EsAudio2.clip = this.Lightning_Sound;
+		this.EsAudio.loop = false;
+
+		StartCoroutine("FootCircle");
+	}
+	
+	
+	IEnumerator FootCircle()
+	{	
+		yield return new WaitForSeconds(1.5f);
+		
+		// 번개 떨어질 자리에 동그라미 표시
+		Instantiate ( Foot_Circle, Char.transform.position, Char.transform.rotation  );
+		
+		FootCirclePos1 = Char.transform.position ;
+		FootCirclePos2 = Char.transform.rotation;
+		
+		StartCoroutine ("Lightning");
+	}
+	
+	
+	IEnumerator Lightning()
+	{
+		yield return new WaitForSeconds(0.5f);
+		
+		// 번개 떨어짐
+		//Instantiate (LightningEffect, Char.transform.position, Char.transform.rotation);
+		
+		Instantiate (LightningEffect, FootCirclePos1, FootCirclePos2 );
+		this.EsAudio2.Play ();
+
+		StartCoroutine ("FootCircle");
+	}
+	
+	
+	
+	
+}
