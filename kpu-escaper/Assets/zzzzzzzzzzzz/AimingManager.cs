44c716da03a1a5357f28eecf25baeb329fd9b38d
900903ksh <900903ksh@naver.com>
2016-08-04 18:02:27 +0900

    에이밍 메인게임에서 경계부분이 명확하게 알파값을 조정하여 드러나도록 함

---
+++ kpu-escaper/Assets/zzzzzzzzzzzz/AimingManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AimingManager : MonoBehaviour {
+
+	public Material BoundaryMaterial;
+	public GameObject Sphere;
+	float fAlpha = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.C)) {
+			//Instantiate (Sphere, Sphere.transform.position, Quaternion.identity);
+			Instantiate (Sphere, new Vector3(Random.Range (0, 3),Random.Range (0, 3),Random.Range (0, 3)), Quaternion.identity);
+		}
+
+
+		if(fAlpha < 1)
+			fAlpha += 0.005f;
+
+
+		BoundaryMaterial.SetColor("_Color", new Color (0, 0, 0, fAlpha));
+	
+	}
+}
---
+++ kpu-escaper/Assets/zzzzzzzzzzzz/SphereControl.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereControl : MonoBehaviour {
+
+	float size = 0.0f;
+	bool max = false;
+
+	// Use this for initialization
+	void Start () {
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!max){
+			size += 0.02f;
+			if(size > 0.7f){
+				max = true;
+			}
+		}else{
+			size -= 0.02f;
+			if(size < 0.0f){
+				Destroy(gameObject);
+			}
+		}
+
+		gameObject.transform.localScale = new Vector3 (size, size, size);
+	
+	}
+}
